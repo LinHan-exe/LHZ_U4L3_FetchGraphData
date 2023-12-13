f5a956e3adc63fd9a6cdcf25888490311497d4e5
= <=>
2023-12-13 14:52:09 -0500

    Finished Lab

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const years = [];
+const temps = [];
+
 async function getData() {
     const response = await fetch("Dataset.csv");
     const data = await response.text();
@@ -5,10 +8,31 @@ async function getData() {
     rows.forEach((elem) => {
         const row = elem.split(",");
         const year = row[0];
-        const temp = row[1];
+        const temp = parseFloat(row[1]) + 14;
+        years.push(year);
+        temps.push(temp);
         console.log(year, temp);
     });
+    await getChart();
 }
-//end of functions
-//call the function to test if you see the image on the left in the console
-    
+
+async function getChart(){
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: years,
+            datasets: [{
+            label: 'Historic Temperatures',
+            borderColor: '#031A6B',
+            backgroundColor: '#031A6B',
+            data: temps,
+            fill: false,
+            borderWidth: 1
+            }]
+        },
+    });
+}
+
+getData();
